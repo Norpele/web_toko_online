4018a5fb85af820affc89acd20d3cd9f511ede07
Norpele <123357969+Norpele@users.noreply.github.com>
2024-11-18 14:14:19 +0700

    Delete app directory

--- app/dashboard.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    // Akses jumlah barang yang ditampilkan di elemen dengan id "jumlah-barang"
-    var jumlahBarang = $('#jumlah-barang').text();
-    console.log("Jumlah jenis barang: " + jumlahBarang);
-});
